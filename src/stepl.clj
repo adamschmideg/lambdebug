8a41cdcba6010d09b8ddd51799b3d0f9f0a160c3
Adam Schmideg <adam@schmideg.net>
2010-07-11 14:55:02 +0200

    Trace path to currently executed form within function -- not working yet

--- src/stepl.clj
+++ src/stepl.clj
@@ -11,7 +11,8 @@
     [clojure.contrib.pprint]
     [clojure.contrib.repl-utils]
     [clojure.contrib.duck-streams]
-    [clojure.contrib [seq-utils :only (indexed)]])
+    [clojure.contrib [seq-utils :only (indexed)]]
+    [stepl debugger])
   (:import [java.util.Date]))
 
 (def *level* 0)
@@ -31,51 +32,59 @@
       ~body))
 
 (defmacro tr
-  [form decorated-form]
-  `(binding [*level* (inc *level*)]
-     (let [indent# (s/join "" (repeat *level* "-"))]
-       (println (str indent# ">") *function* "\t" '~form)
+  [path form decorated-form]
+  `(binding [*level* (inc *level*)
+             *path* (into *path* ~path)]
+     (let [global-indent# (s/join "" (repeat *level* "-"))
+           function-indent# (s/join "" (repeat (count *path*) "*"))] 
+       (println 
+          (str global-indent# ">") "\t" *function*
+          function-indent# "\t" '~form)
        (let [result# ~decorated-form]
-         (println (str indent# "<") *function* "\t" result#)
+         (println 
+            (str global-indent# "<") "\t" *function*
+            function-indent# "\t" "=" result#)
          result#))))
 
 (declare trace-form)
 
 (defn trace-pairs*
   "Decorate even elements of a vector with trace"
-  [pairs]
+  [path pairs]
   (into []
     (map #(if (even? (first %))
             (fnext %)
-            (trace-form (fnext %)))
+            (trace-form (conj path (first %)) (fnext %)))
        (indexed pairs))))
 
 (defn trace-map*
-  [form]
-  `(tr ~form
+  [path form]
+  `(tr ~path ~form
       ~(zipmap
-          (map trace-form (keys form))
-          (map trace-form (vals form)))))
+          (map (fn [[idx frm]] (trace-form [(* 2 idx)] frm))
+            (indexed (keys form)))
+          (map (fn [[idx frm]] (trace-form [(inc (* 2 idx))] frm))
+            (indexed (vals form))))))
 
 (defn trace-set*
-  [form]
-  `(tr ~form 
+  [path form]
+  `(tr ~path ~form 
       ~(set 
-          (for [item form]
-             (trace-form item)))))
+          (for [item (indexed (set-seq form))]
+             (trace-form [(first item)] (second item))))))
 
 (defn trace-list*
-  [form]
-  `(tr ~form
-      ~(for [item form]
-          (trace-form item))))
+  [path form]
+  `(tr ~path ~form
+      ~(for [item (indexed form)]
+          (trace-form [(first item)] (second item)))))
 
 (defn trace-vector*
-  [form]
-  `(tr ~form
+  [path form]
+  `(tr ~path ~form
       ~(into[]
-          (for [item form]
-            (trace-form item)))))
+          (for [item (indexed form)]
+            (trace-form [(first item)] (second item))))))
 
 (defn trace-multi-binding*
   "Trace a macro whose first arg is a vector of
@@ -83,50 +92,52 @@
    traced:
      (let [a b c d] ...) =>
      (let [a (tr b) c (trace d)] ..."
-  [form]
+  [path form]
   (let [func (first form)
         params (fnext form)
         body (nnext form)]
-    `(tr ~form
+    `(tr ~path ~form
         (~func
-         ~(trace-pairs* params)
-         ~@(for [item body]
-            (trace-form item))))))
+         ~(trace-pairs* [1] params)
+         ~@(for [item (indexed body)]
+            (trace-form [2 (first item)] (second item)))))))
 
 (defn trace-single-binding*
   "Trace a macro whose first arg is a param list, like fn*.  Only the
   body is traced:
     (fn* [a b] (inc a)) =>
     (fn* [a b] (tr (inc a) ..."
-  [form]
+  [path form]
   (let [func (first form)
         params (fnext form)
         body (nnext form)]
-    `(tr ~form
+    `(tr ~path ~form
         (~func
          ~params
-         ~@(for [item body]
-            (trace-form item))))))
+         ~@(for [item (indexed body)]
+            (trace-form [2 (first item)] (second item)))))))
 
 (defn trace-rest*
   "Keep the head unchanged, tr only the rest.
    Used for most macros"
-  [form]
-  `(tr ~form
+  [path form]
+  `(tr ~path ~form
      (~(first form)
-      ~@(for [item (next form)]
-         (trace-form item)))))
+      ~@(for [item (indexed (next form))]
+         (trace-form [(inc (first item))] (second item))))))
 
 (defn trace-symbol*
   "Trace symbols, trying to resolve them first, to display a nicer name"
-  [form]
-  `(tr ~form ~(or (resolve form) form)))
+  [path form]
+  `(tr ~path ~form ~(or (resolve form) form)))
 
 (defn trace-primitive*
-  [form]
+  [path form]
   form)
 
-(defn trace-form [form]
+(defn trace-form 
+  ([form] (trace-form nil form))
+  ([path form]
   (cond
     (seq? form)
       (let [func (first form)]
@@ -135,26 +146,26 @@
               (macro? func))
            (condp contains? func
               #{'let 'for 'doseq 'binding}
-                (trace-multi-binding* form)
-              #{'fn*}
-                (trace-single-binding* form)
+                (trace-multi-binding* path form)
+              #{'fn* path}
+                (trace-single-binding* path form)
               ;; default
-                (trace-rest* form))
-           (trace-list* form)))
+                (trace-rest* path form))
+           (trace-list* path form)))
     (map? form)
-      (trace-map* form)
+      (trace-map* path form)
     (vector? form)
-      (trace-vector* form)
+      (trace-vector* path form)
     (set? form)
-      (trace-set* form)
+      (trace-set* path form)
     (symbol? form)
-      (trace-symbol* form)
+      (trace-symbol* path form)
     :else
-      (trace-primitive* form)))
+      (trace-primitive* path form))))
 
 (defn trace-body [name args body]
   (list args 
-     `(enter-function '~name '~args ~(trace-form body))))
+     `(enter-function '~name '~args ~(trace-form 0 body))))
 
 (defn trace-defn [form]
   (let [fn-form (macroexpand-1 form)

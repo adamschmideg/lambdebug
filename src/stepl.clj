a2f1e7583ee7e3155dffd16f45d6fdfb06be2820
Adam Schmideg <adam@schmideg.net>
2010-08-11 19:32:51 +0200

    Remove unused functions from main namespace, leaving only debug there

--- src/stepl.clj
+++ src/stepl.clj
@@ -17,43 +17,6 @@
     [stepl core utils])
   (:import [java.util.Date]))
 
-(defn trace-ns [ns-pattern]
-  (let [namespaces (filter #(.startsWith (str %) ns-pattern)
-                     (loaded-libs))]
-    (doseq [ns namespaces]
-      (do 
-        (doseq [func (keys (ns-publics (symbol ns)))]
-          (println "Tracing" func)
-          (trace-func func))))))
-
-(defn format-trace
-  [trace]
-  (let [widths (apply merge-with #(max %1 
-                                    (cond
-                                      (number? %2)
-                                        %2
-                                      (vector? %2)
-                                        (count %2)
-                                      :else
-                                        (count (str %2))))
-                  {:function 0, :path 0, :level 0, :form 0, :result 0}
-                  trace)
-        template (format "%%-%ss %%-%ss %%-%ss %%-%ss %n" 
-                    (inc (:level widths)) (:function widths)
-                    (inc (:path widths)) (max (:form widths) 
-                                        (:result widths)))]
-      (println "template" template)
-      (map #(format template 
-              (str (repeat-str (:level %) "=")
-                (if (:result %) "<" ">"))
-              (str (:function %))
-              (str (repeat-str (count (:path %)) "-")
-                (if (:result %) "<" ">"))
-              (if (find % :result)
-                (str (:form %) " = " (:result %))
-                (str (:form %))))
-         trace)))
-
 (defn debug
   [form]
   (do
@@ -63,19 +26,3 @@
       (dbg/gui #(= "q" %) 
         (dbg/make-dispatcher (make-steps form) @*function-forms*)
         "i"))))
-
-(defn nice-steps
-  "Trace form and functions used by it.  Functions can be filtered a
-  `wanted` function"
-  ([form]
-    (nice-steps form (complement #(= (.ns %) (find-ns 'clojure.core)))))
-  ([form wanted]
-    (let [vars (traverse used-vars (used-vars-in-form form *ns*))
-          wanted-vars (filter wanted vars)]
-      (doseq [v wanted-vars]
-        (trace-func v))
-      (doseq [line (maps-to-lol
-                    (remove #(find % :result) 
-                      (make-steps form))
-                    [:path :form :function])]
-      (println line)))))

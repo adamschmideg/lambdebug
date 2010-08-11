dc792b298db1f4713126e30a4c7553573daf645c
Adam Schmideg <adam@schmideg.net>
2010-08-11 20:15:10 +0200

    Update project file with namespaces and new version number

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject stepl "0.3.2"
+(defproject lambdebug "0.3.3"
   :description "Step, trace, and debug in clojure repl"
-  :url "https://stepl.taskberry.googlecode.com/hg"
+  :url "git://github.com/adamschmideg/lambdebug.git"
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
   :dev-dependencies [[lein-clojars "0.5.0"]]

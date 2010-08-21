7830fe10f449109dea77df9c732f5303b727aef4
Adam Schmideg <adam@schmideg.net>
2010-08-21 15:22:50 +0200

    Handle multi-line strings when tokenizing a line

--- src/lambdebug/colorizer.clj
+++ src/lambdebug/colorizer.clj
@@ -4,7 +4,9 @@
     [clojure
       [string :as s]]
     [clojure.contrib
-      [string :as string]]))
+      [string :as string]])
+  (:use
+    [lambdebug utils]))
 
 (defn substring-indexes-in-form
   "Interpret s as a form and return 
@@ -26,21 +28,34 @@
     (remove #(= "" %)
       (string/partition #"#\{|[\(\)\{\}\[\]]|[, ]+" line)))
     
+(defn finish-string
+  "Return the part until a quote and the rest"
+  [s]
+    (s/split s #"(?<!\\)\"" 2))
+
 (defn tokenize-line
   "Split a line to tokens, where a token is a string of
    - whitespaces (including comma and comments)
    - a single opening or closing paren
    - a string (which may contain any of the above)
-   - other atom as string"
-  [line]
-    (if-let [string-start-or-comment
-             (re-find #"(.*?)((\"|[ ,]*;)(.*))" line)]
-      (let [[_ before sep+after sep after] string-start-or-comment
-            start (tokenize-simple-line before)]
-        (if (= "\"" sep)
-          (let [[string rest] (s/split after #"(?<!\\)\"" 2)]
-            (if (= "" rest)
-              (concat start [sep+after :cont]))
-              (concat start [(str "\"" string "\"")] (tokenize-line rest)))
-          (concat start [sep+after])))
-      (tokenize-simple-line line)))
+   - other atom as string.
+  If a string is started but not finished on a line,
+  the last token is :cont.  If cont? is true, the line
+  is considered to continue a string from the previous line."
+  [line cont?]
+    (if cont?
+      (let [[string rest] (finish-string line)]
+        (if (= 0 (count rest))
+          [string :cont]
+          (concat [(str string "\"")] (tokenize-line rest false))))
+      (if-let [string-start-or-comment
+               (re-find #"(.*?)((\"|[ ,]*;)(.*))" line)]
+        (let [[_ before sep+after sep after] string-start-or-comment
+              start (tokenize-simple-line before)]
+          (if (= "\"" sep)
+            (let [[string rest] (finish-string after)]
+              (if rest
+                (concat start [(str "\"" string "\"")] (tokenize-line rest false))
+                (concat start [sep+after :cont])))
+            (concat start [sep+after])))
+        (tokenize-simple-line line))))
--- test/lambdebug/colorizer_test.clj
+++ test/lambdebug/colorizer_test.clj
@@ -13,15 +13,26 @@
     "a ( b ) c" [1 0] :=> 4 5))
 )
 
-(deftest tokenize-line-test
-  (are [line _ result]
-    (is (= result (tokenize-line line)))
-    "(+ 2 3)"  :=>  ["(","+"," ","2"," ","3",")"]
-    " ( +  2, ,3)"  :=>  [" ","("," ","+","  ","2", ", ,","3",")"]
-    "\"only string\""  :=>  ["\"only string\""]
-    ";; only comment"  :=>  [";; only comment"]
-    "42 ; answer"  :=>  ["42"," ; answer"]
-    "42 \"answer\" life"  :=>  ["42"," ","\"answer\""," ","life"]
-    "42 \"semi;colon\" life"  :=>  ["42"," ","\"semi;colon\"", " ","life"]
-    "42 ; \"the\" answer"  :=>  ["42", " ; \"the\" answer"]))
+(deftest tokenize-test
+  (testing "Tokenize line"
+    (are [line _ result]
+      (is (= result (tokenize-line line false)))
+      "(+ 2 3)"  :=>  ["(","+"," ","2"," ","3",")"]
+      " ( +  2, ,3)"  :=>  [" ","("," ","+","  ","2", ", ,","3",")"]
+      "\"only string\""  :=>  ["\"only string\""]
+      ";; only comment"  :=>  [";; only comment"]
+      "42 ; answer"  :=>  ["42"," ; answer"]
+      "42 \"answer\" life"  :=>  ["42"," ","\"answer\""," ","life"]
+      "42 \"semi;colon\" life"  :=>  ["42"," ","\"semi;colon\"", " ","life"]
+      "42 ; \"the\" answer"  :=>  ["42", " ; \"the\" answer"]))
+  (testing "Multi-line strings"
+    (is (=
+      (tokenize-line "before \"start string" false)
+      ["before", " ", "\"start string", :cont]))
+    (is (=
+      (tokenize-line "middle of string" true)
+      ["middle of string", :cont]))
+    (is (=
+      (tokenize-line "end string\" after" true)
+      ["end string\"", " ", "after"]))))
 
